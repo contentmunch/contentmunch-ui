3bf6afc54979c3855798b308d55876bbe0a07cdf
contentmuncher <contentmuncher@users.noreply.github.com>
2025-05-26 23:26:46 +0000

    update build

---
+++ docs/assets/IconBadge.stories-DWGOWgII.js
@@ -0,0 +1,28 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{I as n}from"./IconBadge-B0zqyhI9.js";import{B as k}from"./Button-D0_I9hGM.js";import"./index-D4lIrffr.js";import"./Icon-CHNZMfZ6.js";import"./Badge-C4f3BP5R.js";const _={component:n,title:"Data Display/IconBadge",render:B=>o.jsxs("div",{style:{display:"flex",justifyContent:"center",alignItems:"center"},children:[o.jsx(k,{children:"Button"}),o.jsx(n,{...B})]})},r={args:{text:"12",onClick:()=>{console.log("clicked")}}},a={args:{...r.args,variant:"primary"}},e={args:{...r.args,variant:"secondary"}},s={args:{...r.args,variant:"danger"}},t={args:{...r.args,variant:"tertiary"}};var c,i,m;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    text: '12',
+    onClick: () => {
+      console.log("clicked");
+    }
+  }
+}`,...(m=(i=r.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var p,d,g;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    variant: 'primary'
+  }
+}`,...(g=(d=a.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var l,u,y;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    variant: 'secondary'
+  }
+}`,...(y=(u=e.parameters)==null?void 0:u.docs)==null?void 0:y.source}}};var f,x,D;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    variant: 'danger'
+  }
+}`,...(D=(x=s.parameters)==null?void 0:x.docs)==null?void 0:D.source}}};var v,S,j;t.parameters={...t.parameters,docs:{...(v=t.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    variant: 'tertiary'
+  }
+}`,...(j=(S=t.parameters)==null?void 0:S.docs)==null?void 0:j.source}}};const O=["Default","Primary","Secondary","Danger","Tertiary"];export{s as Danger,r as Default,a as Primary,e as Secondary,t as Tertiary,O as __namedExportsOrder,_ as default};
