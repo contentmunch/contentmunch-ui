029a069038b087035405bae1b3f328d84ffd354b
contentmuncher <contentmuncher@users.noreply.github.com>
2025-05-26 21:22:37 +0000

    update build

---
+++ docs/assets/DataTitle.stories-Dhs7UTdh.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";const l={title:"Utils/DataTitle",render:s=>o.jsx("a",{href:"#home",...s,children:"Hover here"})},e={args:{"data-title":"This will show up on hover"}};var r,t,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    'data-title': 'This will show up on hover'
+  }
+}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const n=["Default"];export{e as Default,n as __namedExportsOrder,l as default};
