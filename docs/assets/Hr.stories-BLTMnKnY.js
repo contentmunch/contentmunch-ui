fe49a6271a27f86565aa355adab69c9d5743904c
contentmuncher <contentmuncher@users.noreply.github.com>
2025-05-27 19:41:22 +0000

    update build

---
+++ docs/assets/Hr.stories-BLTMnKnY.js
@@ -0,0 +1,19 @@
+import{j as H}from"./jsx-runtime-D_zvdyIk.js";import{H as t}from"./Hr-wE5IehCy.js";import"./index-D4lIrffr.js";const _={component:t,title:"Rule/Horizontal Rule",render:j=>H.jsx(t,{...j})},r={args:{}},e={args:{variant:"secondary"}},s={args:{style:"dotted"}},a={args:{style:"dashed"}},o={args:{style:"sold"}};var c,n,d;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {}
+}`,...(d=(n=r.parameters)==null?void 0:n.docs)==null?void 0:d.source}}};var m,p,l;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary'
+  }
+}`,...(l=(p=e.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var u,i,g;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    style: 'dotted'
+  }
+}`,...(g=(i=s.parameters)==null?void 0:i.docs)==null?void 0:g.source}}};var y,S,D;a.parameters={...a.parameters,docs:{...(y=a.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    style: 'dashed'
+  }
+}`,...(D=(S=a.parameters)==null?void 0:S.docs)==null?void 0:D.source}}};var f,x,h;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    style: 'sold'
+  }
+}`,...(h=(x=o.parameters)==null?void 0:x.docs)==null?void 0:h.source}}};const z=["Default","Secondary","Dotted","Dashed","Solid"];export{a as Dashed,r as Default,s as Dotted,e as Secondary,o as Solid,z as __namedExportsOrder,_ as default};
