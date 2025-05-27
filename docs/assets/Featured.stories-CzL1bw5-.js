9f36f603582cb998484d37302ee9db7eb0b19b3c
contentmuncher <contentmuncher@users.noreply.github.com>
2025-05-27 20:34:30 +0000

    update build

---
+++ docs/assets/Featured.stories-CzL1bw5-.js
@@ -0,0 +1,15 @@
+import{j as i}from"./jsx-runtime-D_zvdyIk.js";import{F as o}from"./Featured-D1GSdbLo.js";import"./index-D4lIrffr.js";const g={component:o,title:"Utils/Star/Featured",render:d=>i.jsx(o,{...d})},e={args:{isFeatured:!0,onClick:()=>{console.log("clicked")}}},r={args:{isFeatured:!1,onClick:()=>{console.log("clicked")}}};var s,a,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    isFeatured: true,
+    onClick: () => {
+      console.log("clicked");
+    }
+  }
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var n,c,l;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    isFeatured: false,
+    onClick: () => {
+      console.log("clicked");
+    }
+  }
+}`,...(l=(c=r.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};const F=["Default","UnFeatured"];export{e as Default,r as UnFeatured,F as __namedExportsOrder,g as default};
