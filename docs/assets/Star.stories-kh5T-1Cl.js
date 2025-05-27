0cbb683e53679c3dd4147c5feab4b32ceee53a1c
contentmuncher <contentmuncher@users.noreply.github.com>
2025-05-27 20:34:30 +0000

    update build

---
+++ docs/assets/Star.stories-kh5T-1Cl.js
@@ -0,0 +1,20 @@
+import{j as D}from"./jsx-runtime-D_zvdyIk.js";import{S as o}from"./Star-2LGqRjU-.js";import"./index-D4lIrffr.js";const G={component:o,title:"Utils/Star",render:x=>D.jsx(o,{...x})},r={args:{rating:3.4}},e={args:{...r.args,size:"small"}},s={args:{...r.args,size:"medium"}},a={args:{...r.args,color:"green"}};var t,n,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    rating: 3.4
+  }
+}`,...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};var c,i,p;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    size: 'small'
+  }
+}`,...(p=(i=e.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var u,g,l;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    size: 'medium'
+  }
+}`,...(l=(g=s.parameters)==null?void 0:g.docs)==null?void 0:l.source}}};var d,S,f;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    color: 'green'
+  }
+}`,...(f=(S=a.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};const M=["Default","Small","Medium","Green"];export{r as Default,a as Green,s as Medium,e as Small,M as __namedExportsOrder,G as default};
