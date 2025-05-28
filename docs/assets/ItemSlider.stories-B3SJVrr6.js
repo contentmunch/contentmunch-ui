1898b1aa262884ec88b6d6f4ee60c339871fd272
contentmuncher <contentmuncher@users.noreply.github.com>
2025-05-28 22:16:04 +0000

    update build

---
+++ docs/assets/ItemSlider.stories-B3SJVrr6.js
@@ -0,0 +1,26 @@
+import{j as s}from"./jsx-runtime-D_zvdyIk.js";/* empty css                            */import{I as i}from"./ItemSlider-GOT6HZYo.js";import"./index-D4lIrffr.js";import"./NavigateButton-D9M7PrA7.js";import"./Button-BkaHsfYs.js";import"./Icon-Bj0efVpV.js";const E={component:i,title:"Slider/Item Slider",decorators:[o=>s.jsx("div",{className:"container",children:s.jsx("div",{className:"slider-story",children:s.jsx(o,{})})})],render:o=>s.jsx(i,{...o})},a={args:{sliderItems:[s.jsx("div",{className:"slider-story--item",children:s.jsx("img",{src:"https://www.renaissancerentals.com/api/asset/download/1s5-NPYxlB5NRsMMhX8MB-pDsN1bqEoZ_",className:"slide"})}),s.jsx("div",{className:"slider-story--item",children:s.jsx("img",{src:"https://www.renaissancerentals.com/api/asset/download/1n0NNoyBnzt-nfECziwiuPxz7k-InaMq_",className:"slide"})}),s.jsx("div",{className:"slider-story--item",children:s.jsx("img",{src:"https://www.renaissancerentals.com/api/asset/download/1rtcmnLUABimyLpQ6kWm2GD3kJLNMBjKB",className:"slide"})})]}},e={args:{...a.args,navIcon:"arrow"}},r={args:{...a.args,navIcon:"arrow",navButtonWeight:1,variant:"transparent"}},t={args:{sliderItems:[s.jsx("img",{src:"https://www.renaissancerentals.com/api/asset/download/1n0NNoyBnzt-nfECziwiuPxz7k-InaMq_",className:"slide",width:"500",height:"300"})]}},n={args:{...a.args,navButtonPlacement:"outside"}};var c,m,d;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    sliderItems: [<div className="slider-story--item"><img src="https://www.renaissancerentals.com/api/asset/download/1s5-NPYxlB5NRsMMhX8MB-pDsN1bqEoZ_" className="slide" /></div>, <div className="slider-story--item"><img src="https://www.renaissancerentals.com/api/asset/download/1n0NNoyBnzt-nfECziwiuPxz7k-InaMq_" className="slide" /></div>, <div className="slider-story--item"><img src="https://www.renaissancerentals.com/api/asset/download/1rtcmnLUABimyLpQ6kWm2GD3kJLNMBjKB" className="slide" /></div>]
+  }
+}`,...(d=(m=a.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var l,p,w;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    navIcon: "arrow"
+  }
+}`,...(w=(p=e.parameters)==null?void 0:p.docs)==null?void 0:w.source}}};var u,g,N;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    navIcon: "arrow",
+    navButtonWeight: 1,
+    variant: "transparent"
+  }
+}`,...(N=(g=r.parameters)==null?void 0:g.docs)==null?void 0:N.source}}};var h,v,x;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    sliderItems: [<img src="https://www.renaissancerentals.com/api/asset/download/1n0NNoyBnzt-nfECziwiuPxz7k-InaMq_" className="slide" width="500" height="300" />]
+  }
+}`,...(x=(v=t.parameters)==null?void 0:v.docs)==null?void 0:x.source}}};var B,I,j;n.parameters={...n.parameters,docs:{...(B=n.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    navButtonPlacement: "outside"
+  }
+}`,...(j=(I=n.parameters)==null?void 0:I.docs)==null?void 0:j.source}}};const P=["Default","WithArrowIcon","Tranparent","SingleItem","NavButtonOutside"];export{a as Default,n as NavButtonOutside,t as SingleItem,r as Tranparent,e as WithArrowIcon,P as __namedExportsOrder,E as default};
