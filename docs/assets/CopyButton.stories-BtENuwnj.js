6193445266f39623b152e406b0665230ef192bdc
contentmuncher <contentmuncher@users.noreply.github.com>
2025-05-28 20:37:01 +0000

    update build

---
+++ docs/assets/CopyButton.stories-BtENuwnj.js
@@ -0,0 +1,5 @@
+import{j as m}from"./jsx-runtime-D_zvdyIk.js";import{C as r}from"./CopyButton-CglhABFg.js";import"./index-D4lIrffr.js";import"./Button-BkaHsfYs.js";import"./Icon-Bj0efVpV.js";const d={component:r,title:"Buttons/Copy Button",render:a=>m.jsx(r,{...a})},t={args:{text:"This is the text from clipboard"}};var o,e,s;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    text: "This is the text from clipboard"
+  }
+}`,...(s=(e=t.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const x=["Default"];export{t as Default,x as __namedExportsOrder,d as default};
