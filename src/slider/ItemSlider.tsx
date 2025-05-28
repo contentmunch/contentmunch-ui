0ae13e3cad247e894ea7ce9a5d3a4312a8cb1a81
contentmuncher <890395+asikpradhan@users.noreply.github.com>
2025-05-28 18:14:38 -0400

    feat: navButton placement

--- src/slider/ItemSlider.tsx
+++ src/slider/ItemSlider.tsx
@@ -5,56 +5,63 @@ import type {Size} from "../common/Size.ts";
 import type {Variant} from "../common/Variant.ts";
 
 export const ItemSlider: React.FC<ItemSliderProps> = (
-    {sliderItems, navButtonSize = 'large', navButtonWeight = 2, variant, navIcon}) => {
+    {sliderItems, navButtonSize = 'large', navButtonWeight = 2, variant, navIcon, navButtonPlacement = 'inside'}) => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [translate, setTranslate] = useState(0);
 
 
+    const renderLeftButton = () =>
+        sliderItems.length > 1 && currentIndex > 0 ?
+            <NavigateButton direction="left" size={navButtonSize}
+                            variant={variant}
+                            navIcon={navIcon} weight={navButtonWeight}
+                            onClick={() => {
+                                if (currentIndex === 0) {
+                                    setTranslate(-(sliderItems.length - 1) * 100)
+                                    setCurrentIndex(sliderItems.length - 1);
+                                } else {
+
+                                    setTranslate(translate + 100);
+                                    setCurrentIndex(currentIndex - 1);
+                                }
+
+                            }}/> : ""
+
+    const renderRightButton = () => sliderItems.length > 1 && currentIndex < sliderItems.length - 1 ?
+        <NavigateButton direction="right" size={navButtonSize} weight={navButtonWeight}
+                        navIcon={navIcon}
+                        variant={variant}
+                        onClick={() => {
+                            if (currentIndex === sliderItems.length - 1) {
+                                setCurrentIndex(0);
+                                setTranslate(0);
+
+                            } else {
+                                setTranslate(translate - 100);
+                                setCurrentIndex(currentIndex + 1);
+                            }
+
+                        }}/> : ""
+
     return (
-        <div className="muncher-item-slider">
-            {sliderItems.length > 1 && currentIndex > 0 ?
-                <NavigateButton direction="left" size={navButtonSize}
-                                variant={variant}
-                                navIcon={navIcon} weight={navButtonWeight}
-                                onClick={() => {
-                                    if (currentIndex === 0) {
-                                        setTranslate(-(sliderItems.length - 1) * 100)
-                                        setCurrentIndex(sliderItems.length - 1);
-                                    } else {
-
-                                        setTranslate(translate + 100);
-                                        setCurrentIndex(currentIndex - 1);
-                                    }
-
-                                }}/> : ""}
-
-            <div className="slider-container" style={{width: `${100 * sliderItems.length}%`}}>
-                {sliderItems.map((sliderItem, index) =>
-                    <div className="slider-item"
-                         style={{transform: `translateX(${translate}%)`}}
-                         key={"slider-item" + index}>
-                        {
-                            sliderItem
-                        }
-                    </div>
-                )}
+        <div className={"muncher-item-slider-wrapper--" + navButtonPlacement}>
+            {navButtonPlacement === 'outside' ? renderLeftButton() : ""}
+            <div className="muncher-item-slider">
+                {navButtonPlacement === 'inside' ? renderLeftButton() : ""}
+                <div className="slider-container" style={{width: `${100 * sliderItems.length}%`}}>
+                    {sliderItems.map((sliderItem, index) =>
+                        <div className="slider-item"
+                             style={{transform: `translateX(${translate}%)`}}
+                             key={"slider-item" + index}>
+                            {
+                                sliderItem
+                            }
+                        </div>
+                    )}
+                </div>
+                {navButtonPlacement === 'inside' ? renderRightButton() : ""}
             </div>
-            {sliderItems.length > 1 && currentIndex < sliderItems.length - 1 ?
-                <NavigateButton direction="right" size={navButtonSize} weight={navButtonWeight}
-                                navIcon={navIcon}
-                                variant={variant}
-                                onClick={() => {
-                                    if (currentIndex === sliderItems.length - 1) {
-                                        setCurrentIndex(0);
-                                        setTranslate(0);
-
-                                    } else {
-                                        setTranslate(translate - 100);
-                                        setCurrentIndex(currentIndex + 1);
-                                    }
-
-                                }}/> : ""}
-
+            {navButtonPlacement === 'outside' ? renderRightButton() : ""}
         </div>
     );
 };
@@ -65,5 +72,6 @@ export interface ItemSliderProps {
     navButtonWeight?: 1 | 2 | 3;
     navIcon?: NavIcon;
     variant?: Variant;
+    navButtonPlacement?: 'inside' | 'outside';
 }
 
