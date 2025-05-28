dd0f619a171366bbc0bfa5162860cdfc01b5d96f
contentmuncher <contentmuncher@users.noreply.github.com>
2025-05-28 23:47:19 +0000

    update build

--- docs/assets/Radio.stories-C_9_Bnui.js
+++
@@ -1,6 +0,0 @@
-import{j as m}from"./jsx-runtime-D_zvdyIk.js";import{R as a}from"./Radio-BhT_AO0L.js";import"./index-D4lIrffr.js";/* empty css              */import"./Label-WHfwOnzY.js";const u={component:a,title:"Input/Radio",render:s=>m.jsx(a,{...s})},r={args:{name:"muncher-radio",label:"radio"}};var e,o,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    name: 'muncher-radio',
-    label: 'radio'
-  }
-}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const l=["Default"];export{r as Default,l as __namedExportsOrder,u as default};
