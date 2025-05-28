5d555d5bd6e001e94bdad42c82a94f83de39b510
contentmuncher <contentmuncher@users.noreply.github.com>
2025-05-28 19:38:18 +0000

    update build

---
+++ docs/assets/ImageInput.stories-Bg62k9nN.js
@@ -0,0 +1,9 @@
+import{j as m}from"./jsx-runtime-D_zvdyIk.js";import{I as r}from"./ImageInput-i5bs7LB5.js";import"./index-D4lIrffr.js";/* empty css                   */import"./Icon-Bj0efVpV.js";const d={component:r,title:"Input/Image Input",render:o=>m.jsx(r,{...o})},e={args:{name:"file",label:"upload image",required:!0,multiple:!0,setFiles:()=>{}}};var t,a,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    name: "file",
+    label: "upload image",
+    required: true,
+    multiple: true,
+    setFiles: () => {}
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,d as default};
