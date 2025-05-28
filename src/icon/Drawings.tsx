3f1f4948f13c335db6774344348594ed873f6f6f
contentmuncher <asikpradhan@gmail.com>
2025-05-28 15:36:43 -0400

    feat: add folder icon

--- src/icon/Drawings.tsx
+++ src/icon/Drawings.tsx
@@ -321,6 +321,10 @@ export const drawings = {
                 <path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"/>
                 <polyline points="13 2 13 9 20 9"/>
             </Fragment>,
+        'folder':
+            <Fragment>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/>
+            </Fragment>,
         'terminal':
             <Fragment>
                 <polyline points="4 17 10 11 4 5"/>
