1e5c3c2a823306fecb57c285f7097e59c810d649
contentmuncher <asikpradhan@gmail.com>
2025-08-23 19:41:46 -0400

    feat: cleaner radio

--- src/input/Radio.tsx
+++ src/input/Radio.tsx
@@ -1,29 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './assets/input.css';
 import {Label} from './Label';
 
 export const Radio: React.FC<RadioProps> = ({name, required = false, onChange, checked = false, label, ...props}) => {
-    const [radioChecked, setRadioChecked] = useState(checked ? checked : false);
 
     return (
         <div className="muncher-input--div">
             <div className="muncher-radio">
                 <input
-                    id={name}
                     type="radio"
                     name={name}
-                    onChange={event => {
-                        setRadioChecked(event.target.checked);
-                        if (onChange) {
-                            onChange();
-                        }
-                    }}
-                    checked={radioChecked}
+                    onChange={onChange}
+                    checked={checked}
                     {...props}
                 />
-                {label ? <Label label={label} required={required} id={name}
+                {label ? <Label label={label} required={required}
                                 onClick={() => {
-                                    setRadioChecked(!radioChecked);
                                     if (onChange) {
                                         onChange();
                                     }
