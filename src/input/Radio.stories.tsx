5658a3c48d2e706c50b7789e3ad0bed4d5be531e
contentmuncher <890395+asikpradhan@users.noreply.github.com>
2025-08-23 19:41:46 -0400

    feat: cleaner radio

--- src/input/Radio.stories.tsx
+++ src/input/Radio.stories.tsx
@@ -1,17 +1,48 @@
-import type {Meta, StoryObj} from '@storybook/react';
+import type {Meta, StoryFn} from '@storybook/react';
 import {Radio} from "./Radio";
+import {useState} from "react";
 
-const meta: Meta<typeof Radio> = {
-    component: Radio,
+
+const meta = {
     title: 'Input/Radio',
-    render: (args) => <Radio {...args}/>,
-};
+    component: Radio,
+    decorators: [
+        (Story) => (
+            <div style={{width: '95vw'}}>
+                <Story/>
+            </div>)
+    ],
+} satisfies Meta<typeof Radio>;
 
 export default meta;
-type Story = StoryObj<typeof Radio>;
-export const Default: Story = {
-    args: {
-        name: 'muncher-radio',
-        label: 'radio',
+type Story = StoryFn<typeof meta>;
+
+export const Default: Story = () => {
+    const [checkedAnswer, setCheckedAnswer] = useState<string>("");
+
+    const handleAnswerChecked = (answer: string) => {
+        if (checkedAnswer === answer) {
+            setCheckedAnswer("");
+        } else {
+            setCheckedAnswer(answer);
+        }
     }
+    return (
+        <div>
+            <Radio name="question-option"
+                   checked={checkedAnswer === "Image Processing"}
+                   onChange={() => handleAnswerChecked("Image Processing")}
+                   label="Image Processing"/>
+            <Radio name="question-option"
+                   checked={checkedAnswer === "File Storage"}
+                   onChange={() => handleAnswerChecked("File Storage")}
+                   label="File Storage"/>
+            <Radio name="question-option"
+                   checked={checkedAnswer === "Distributed Image Streaming"}
+                   onChange={() => handleAnswerChecked("Distributed Image Streaming")}
+                   label="Distributed Image Streaming"/>
+
+        </div>
+
+    )
 };
