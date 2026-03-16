1efcec2d9c514e7f6ed9333635038093163fd50d
contentmuncher <contentmuncher@users.noreply.github.com>
2026-03-16 00:40:17 +0000

    update build

---
+++ docs/assets/CopyButton.stories-Bw_cVFKW.js
@@ -0,0 +1,5 @@
+import{j as m}from"./jsx-runtime-D_zvdyIk.js";import{C as r}from"./CopyButton-2Sp5_-Xw.js";import"./index-D4lIrffr.js";import"./Button-BkaHsfYs.js";import"./Icon-B5B9q2Wi.js";const d={component:r,title:"Buttons/Copy Button",render:a=>m.jsx(r,{...a})},t={args:{text:"This is the text from clipboard"}};var o,e,s;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    text: "This is the text from clipboard"
+  }
+}`,...(s=(e=t.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const x=["Default"];export{t as Default,x as __namedExportsOrder,d as default};
---
+++ docs/assets/RoundedBadge.stories-Bg9ZnGEv.js
@@ -0,0 +1,12 @@
+import{j as m}from"./jsx-runtime-D_zvdyIk.js";import{B as a}from"./Badge-DyX1sYip.js";import"./index-D4lIrffr.js";const l={component:a,title:"Data Display/Badge",render:u=>m.jsx(a,{...u,children:"1"})},r={args:{variant:"primary",rounded:!0}},e={args:{variant:"transparent",rounded:!0,border:!0}};var t,o,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary',
+    rounded: true
+  }
+}`,...(n=(o=r.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var s,d,p;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    variant: 'transparent',
+    rounded: true,
+    border: true
+  }
+}`,...(p=(d=e.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};const x=["Rounded","Bordered"];export{e as Bordered,r as Rounded,x as __namedExportsOrder,l as default};
