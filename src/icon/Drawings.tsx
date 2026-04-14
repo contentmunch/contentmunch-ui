5e88c4e0c99695705f45f9f359b1f2b334fefa76
contentmuncher <contentmunch@gmail.com>
2026-04-14 13:45:06 -0400

    feat: split view icon

--- src/icon/Drawings.tsx
+++ src/icon/Drawings.tsx
@@ -62,6 +62,11 @@ export const drawings = {
                 <line x1="3" y1="15" x2="21" y2="15"/>
                 <line x1="12" y1="3" x2="12" y2="21"/>
             </Fragment>,
+        'split-view':
+            <Fragment>
+                <rect x="3" y="3" width="18" height="18" rx="2" ry="2"/>
+                <line x1="12" y1="3" x2="12" y2="21"/>
+            </Fragment>,
         'undo':
             <Fragment>
                 <polyline points="2 10 2 16 8 16"/>
