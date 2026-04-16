bbb38af2a5f8f18b5ee0bda8c3852f9ef3dcbdd1
contentmuncher <contentmunch@gmail.com>
2026-04-15 20:09:31 -0400

    fix: long text icon shrinkage

--- src/list/Ul.stories.tsx
+++ src/list/Ul.stories.tsx
@@ -10,7 +10,7 @@ const meta: Meta<typeof Ul> = {
         <Ul>
             <Li isFeatured={true}>Bonus room</Li>
             <Li bulletColor="green" bulletIcon="check">Gourmet kitchen</Li>
-            <Li>Wood floors</Li>
+            <Li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Li>
             <Li bulletIcon="chevron-right">All-electric</Li>
             <Li>On-site recycling</Li>
         </Ul>
