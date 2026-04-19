97fa0bcb4c80cb94aa3654faea1419797cdf08ba
contentmuncher <contentmuncher@users.noreply.github.com>
2026-04-19 16:55:07 +0000

    update build

---
+++ docs/assets/Card-sHLkWQUK.js
@@ -0,0 +1,1 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import"./index-D4lIrffr.js";const t=({children:e})=>r.jsx("div",{className:"muncher-card",children:e});t.__docgenInfo={description:"",methods:[],displayName:"Card",props:{children:{required:!0,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}}};export{t as C};
