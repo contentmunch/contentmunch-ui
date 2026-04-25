44a36a194a8c6bd885f876df83d41bf267945918
contentmuncher <890395+asikpradhan@users.noreply.github.com>
2026-04-25 13:37:30 -0400

    feat: default sort order

--- src/table/Table.stories.tsx
+++ src/table/Table.stories.tsx
@@ -24,46 +24,17 @@ export const Default: Story = () =>
             {name: "Name", title: "First name and Last name"},
             {name: "Email", title: "Email Address", visibility: "large-screen"},
             {name: "Phone", title: "Phone Number", visibility: "large-screen"},
-            {name: "Eligibility", title: "Eligibility"},
+            {name: <b>Not Sortable</b>, title: "Not Sortable"},
             {name: <Icon name="plus"/>, csvExclude: true},
             {name: "Employee", title: " Employee", visibility: "hidden"},
 
         ], rows: [
-            [
-                {value: "1", content: <a href="google.com">1</a>},
-                {value: "James Dean", content: "James Dean"},
-                {
-                    value: "jamesdean@gmail.com",
-                    content: <a href="mailto:jamesdean@gmail.com">jamesdean@gmail.com</a>
-                },
-                {value: "123456", content: <a href="test:123456">123-456</a>},
-                {value: "false", csv: "no", content: "No"},
-                {value: "1", content: <Icon name="plus"/>},
-                {value: "employee", csv: "yes", content: "Yes"},
-            ],
+
             [
                 {value: "2", content: <a href="google.com">2</a>},
                 {value: "Dean Smith", content: "Dean Smith"},
                 {value: "dean@gmail.com", content: <a href="mailto:dean@gmail.com">dean@gmail.com</a>},
                 {value: "111456", content: <a href="test:111456">111-456</a>},
-                {value: "true", csv: "yes", content: "Yes"},
-                {value: "1", content: <Icon name="plus"/>},
-                {value: "employee", csv: "yes", content: "Yes"},
-            ],
-            [
-                {value: "3", content: <a href="google.com">3</a>},
-                {value: "James Buckner", content: "James Buckner"},
-                {value: "james@gmail.com", content: <a href="mailto:james@gmail.com">james@gmail.com</a>},
-                {value: "123777", content: <a href="test:123777">123-777</a>},
-                {value: "true", csv: "yes", content: "Yes"},
-                {value: "1", content: <Icon name="plus"/>},
-                {value: "employee", csv: "yes", content: "Yes"},
-            ],
-            [
-                {value: "4", content: <a href="google.com">4</a>},
-                {value: "Robert Muller", content: "Robert Muller"},
-                {value: "muller@gmail.com", content: <a href="mailto:muller@gmail.com">muller@gmail.com</a>},
-                {value: "112576", content: <a href="test:112576">112-576</a>},
                 {value: "true", csv: "yes", content: "Yes"},
                 {value: "1", content: <Icon name="plus"/>},
                 {value: "employee", csv: "yes", content: "Yes"},
@@ -77,6 +48,37 @@ export const Default: Story = () =>
                 {value: "1", content: <Icon name="plus"/>},
                 {value: "employee", csv: "no", content: "No"},
             ],
+            [
+                {value: "3", content: <a href="google.com">3</a>},
+                {value: "James Buckner", content: "James Buckner"},
+                {value: "james@gmail.com", content: <a href="mailto:james@gmail.com">james@gmail.com</a>},
+                {value: "123777", content: <a href="test:123777">123-777</a>},
+                {value: "true", csv: "yes", content: "Yes"},
+                {value: "1", content: <Icon name="plus"/>},
+                {value: "employee", csv: "yes", content: "Yes"},
+            ],
+            [
+                {value: "1", content: <a href="google.com">1</a>},
+                {value: "James Dean", content: "James Dean"},
+                {
+                    value: "jamesdean@gmail.com",
+                    content: <a href="mailto:jamesdean@gmail.com">jamesdean@gmail.com</a>
+                },
+                {value: "123456", content: <a href="test:123456">123-456</a>},
+                {value: "false", csv: "no", content: "No"},
+                {value: "1", content: <Icon name="plus"/>},
+                {value: "employee", csv: "yes", content: "Yes"},
+            ],
+            [
+                {value: "4", content: <a href="google.com">4</a>},
+                {value: "Robert Muller", content: "Robert Muller"},
+                {value: "muller@gmail.com", content: <a href="mailto:muller@gmail.com">muller@gmail.com</a>},
+                {value: "112576", content: <a href="test:112576">112-576</a>},
+                {value: "true", csv: "yes", content: "Yes"},
+                {value: "1", content: <Icon name="plus"/>},
+                {value: "employee", csv: "yes", content: "Yes"},
+            ],
+
         ]
     }}</Table>;
 const generatePhone = (): Col => {
