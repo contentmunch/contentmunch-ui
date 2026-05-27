96c73ea74da1b2b552739f0bddaa593bc7f64da1
contentmuncher <890395+asikpradhan@users.noreply.github.com>
2026-05-27 18:11:49 -0400

    feat: more icons (v1.1.42)

--- src/icon/Drawings.tsx
+++ src/icon/Drawings.tsx
@@ -1515,6 +1515,22 @@ export const drawings = {
                 <path d="M3 10h18"/>
                 <path d="M8 2v4"/>
             </Fragment>,
+        'calendar-food':
+            <Fragment>
+                <path d="M16 2v4"/>
+                <path d="M8 2v4"/>
+                <path d="M3 10h18"/>
+                <path d="M21 10.592V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h6"/>
+                <g transform="translate(11.5, 11.5) scale(0.48)" stroke-width="3">
+                    <path d="M7 21h10"/>
+                    <path d="M12 21a9 9 0 0 0 9-9H3a9 9 0 0 0 9 9Z"/>
+                    <path
+                        d="M11.38 12a2.4 2.4 0 0 1-.4-4.77 2.4 2.4 0 0 1 3.2-2.77 2.4 2.4 0 0 1 3.47-.63 2.4 2.4 0 0 1 3.37 3.37 2.4 2.4 0 0 1-1.1 3.7 2.51 2.51 0 0 1 .03 1.1"
+                       />
+                    <path d="m13 12 4-4"/>
+                    <path d="M10.9 7.25A3.99 3.99 0 0 0 4 10c0 .73.2 1.41.54 2"/>
+                </g>
+            </Fragment>,
         'a-z':
             <Fragment>
                 <path d="m3 16 4 4 4-4"/>
