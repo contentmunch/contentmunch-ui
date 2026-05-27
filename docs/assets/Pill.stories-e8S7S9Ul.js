c6864364b784c4315c7c1ab68c6703ecaace0336
contentmuncher <asikpradhan@gmail.com>
2026-05-27 18:11:49 -0400

    feat: more icons (v1.1.42)

---
+++ docs/assets/Pill.stories-e8S7S9Ul.js
@@ -0,0 +1,21 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{r as u}from"./index-D4lIrffr.js";import{P as m}from"./Modal-Dv96XT8U.js";import{B as P}from"./Button-C9pFy32U.js";/* empty css              */import"./Accordion-CM44lgGc.js";import"./Backdrop-B_dFhGSg.js";import"./Badge-DyX1sYip.js";import"./IconBadge-CUW6F8i0.js";import"./Icon-DhU3QS1W.js";import"./DropdownButton-CmU7SL_W.js";import"./DropdownNavButton-emYMPrug.js";import"./ShareButton-BUEqNhqf.js";import"./CopyButton-BEHfnWTq.js";import"./CsvButton-Cd-ZbeCJ.js";import"./SortButton-BlJ2nMh-.js";import"./NavigateButton-kfTbBqsp.js";import"./Checkbox-3yzDZ_xH.js";/* empty css              */import"./Label-WHfwOnzY.js";import"./CsvInput-Dtflanti.js";/* empty css                   */import"./Spinner-Bp0FQ2cK.js";import"./ImageInput-CyYt1kHk.js";import"./Input-ClOswPGi.js";import"./PasswordInput-DSXPLTDW.js";import"./Radio-CzMvCZtD.js";import"./Select-CvzxcGzD.js";import"./Textarea-C755UQ6c.js";import"./TextInputDropdown-AoI4GRlG.js";import"./Li-B51NmS8k.js";import"./Paper-7Qt7Ew3B.js";import"./Progress-CdK5Z50d.js";import"./Hr-DywUS52c.js";import"./ItemSlider-Du8s9D3F.js";import"./RangeSlider-Bl7iMCYB.js";/* empty css               */import"./Slider-CI6O3bQt.js";import"./Featured-D1GSdbLo.js";import"./Star-2LGqRjU-.js";import"./Tabs-De-YnFzz.js";import"./Table-BddkWp-L.js";import"./Card-sHLkWQUK.js";import"./FlippableCard-DWER4QIe.js";import"./ScrollFab-aT8vgVRM.js";const pt={component:m,title:"Data Display/Pill"},i=()=>{const[l,s]=u.useState(["Pill 1"]),c=()=>{const t=[...l];t.push("Pill "+(t.length+1)),s(t)},a=t=>{const o=[...l],d=o.indexOf(t);o.splice(d,1),s(o)};return r.jsxs("div",{className:"muncher-pill-story",children:[r.jsx(P,{variant:"tertiary",onClick:c,children:"Add pills"}),r.jsx("br",{}),r.jsx("br",{}),l.map(t=>r.jsx(m,{pillCloseHandler:()=>a(t),children:t},t))]})};i.__docgenInfo={description:"",methods:[],displayName:"Default"};var p,n,e;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  const [pills, setPills] = useState(["Pill 1"]);
+  const addPills = () => {
+    const currentPills = [...pills];
+    currentPills.push("Pill " + (currentPills.length + 1));
+    setPills(currentPills);
+  };
+  const removePill = (pill: string) => {
+    const currentPills = [...pills];
+    const index = currentPills.indexOf(pill);
+    currentPills.splice(index, 1);
+    setPills(currentPills);
+  };
+  return <div className="muncher-pill-story">
+
+            <Button variant={"tertiary"} onClick={addPills}>Add pills</Button>
+            <br /><br />
+            {pills.map(pill => <Pill key={pill} pillCloseHandler={() => removePill(pill)}>{pill}</Pill>)}
+
+        </div>;
+}`,...(e=(n=i.parameters)==null?void 0:n.docs)==null?void 0:e.source}}};const nt=["Default"];export{i as Default,nt as __namedExportsOrder,pt as default};
