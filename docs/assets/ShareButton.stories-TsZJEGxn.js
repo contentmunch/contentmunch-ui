99070fa2568aed72a51d66b7044792b63e8bb64e
contentmuncher <890395+asikpradhan@users.noreply.github.com>
2026-07-14 17:14:14 -0400

    feat: boiler (v1.1.56)

--- docs/assets/ShareButton.stories-TsZJEGxn.js
+++
@@ -1,5 +0,0 @@
-import{j as m}from"./jsx-runtime-D_zvdyIk.js";import{S as r}from"./ShareButton-C6bEospE.js";import"./index-D4lIrffr.js";import"./Button-CpAfKxsB.js";import"./DropdownButton-irs1yMoW.js";import"./Icon--wLlAnc8.js";import"./CopyButton-jSh7ClxP.js";const h={component:r,title:"Buttons/Share Button",render:s=>m.jsx(r,{...s})},t={args:{title:"Check out this apartment"}};var e,o,a;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    title: "Check out this apartment"
-  }
-}`,...(a=(o=t.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const f=["Default"];export{t as Default,f as __namedExportsOrder,h as default};
