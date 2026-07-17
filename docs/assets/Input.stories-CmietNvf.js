d2aa1e34d0d8dff30ca3b14e5060f5b9a014f144
contentmuncher <890395+asikpradhan@users.noreply.github.com>
2026-07-16 21:54:43 -0400

    feat: life-ops, project (v1.1.59)

---
+++ docs/assets/Input.stories-CmietNvf.js
@@ -0,0 +1,134 @@
+import{j as I}from"./jsx-runtime-D_zvdyIk.js";/* empty css                    */import{I as v}from"./Input-Cqf7JKmc.js";import"./index-D4lIrffr.js";/* empty css              */import"./Label-WHfwOnzY.js";import"./Icon-1_RwmGu3.js";import"./Spinner-Bp0FQ2cK.js";const Qe={component:v,title:"Input/Input",decorators:[W=>I.jsx("div",{className:"div-input-stories",children:I.jsx(W,{})})],render:W=>I.jsx(v,{...W})},e={args:{name:"query",type:"text",placeholder:"Type your query"}},r={args:{...e.args,label:"Search",required:!0}},s={args:{...e.args,label:"Search the string",required:!0,labelPosition:"side"}},a={args:{...r.args,icon:"muncher"}},o={args:{...r.args,hoverIcon:"muncher",onHoverIconClick:()=>{console.log("hover icon clicked")}}},t={args:{...a.args,error:"Provide value for Search"}},n={args:{...a.args,information:"The Value comes from title"}},c={args:{...e.args,type:"color"}},p={args:{...e.args,type:"date"}},m={args:{...e.args,type:"datetime-local"}},i={args:{...e.args,type:"email",placeholder:"Email",icon:"mail"}},l={args:{...e.args,type:"month"}},u={args:{...e.args,type:"number",placeholder:"Phone number",icon:"phone"}},g={args:{...e.args,type:"number",step:.5,placeholder:"Floating point number"}},d={args:{...e.args,type:"password",placeholder:"Type password"}},h={args:{...e.args,type:"range"}},y={args:{...e.args,type:"search"}},S={args:{...e.args,type:"tel"}},f={args:{...e.args,type:"time"}},T={args:{...e.args,type:"url",placeholder:"type url"}},D={args:{...e.args,type:"week"}},b={args:{...e.args,onEnterPress:()=>{console.log("Enter pressed")}}},x={args:{...e.args,displayLoader:!0,icon:"search"}};var E,P,k;e.parameters={...e.parameters,docs:{...(E=e.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    name: "query",
+    type: "text",
+    placeholder: "Type your query"
+  }
+}`,...(k=(P=e.parameters)==null?void 0:P.docs)==null?void 0:k.source}}};var q,w,L;r.parameters={...r.parameters,docs:{...(q=r.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    label: 'Search',
+    required: true
+  }
+}`,...(L=(w=r.parameters)==null?void 0:w.docs)==null?void 0:L.source}}};var j,C,F;s.parameters={...s.parameters,docs:{...(j=s.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    label: 'Search the string',
+    required: true,
+    labelPosition: 'side'
+  }
+}`,...(F=(C=s.parameters)==null?void 0:C.docs)==null?void 0:F.source}}};var H,N,O;a.parameters={...a.parameters,docs:{...(H=a.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    ...TextWithLabel.args,
+    icon: "muncher"
+  }
+}`,...(O=(N=a.parameters)==null?void 0:N.docs)==null?void 0:O.source}}};var R,M,U;o.parameters={...o.parameters,docs:{...(R=o.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    ...TextWithLabel.args,
+    hoverIcon: "muncher",
+    onHoverIconClick: () => {
+      console.log("hover icon clicked");
+    }
+  }
+}`,...(U=(M=o.parameters)==null?void 0:M.docs)==null?void 0:U.source}}};var V,_,z;t.parameters={...t.parameters,docs:{...(V=t.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    ...TextWithIcon.args,
+    error: "Provide value for Search"
+  }
+}`,...(z=(_=t.parameters)==null?void 0:_.docs)==null?void 0:z.source}}};var A,B,G;n.parameters={...n.parameters,docs:{...(A=n.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    ...TextWithIcon.args,
+    information: "The Value comes from title"
+  }
+}`,...(G=(B=n.parameters)==null?void 0:B.docs)==null?void 0:G.source}}};var J,K,Q;c.parameters={...c.parameters,docs:{...(J=c.parameters)==null?void 0:J.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    type: 'color'
+  }
+}`,...(Q=(K=c.parameters)==null?void 0:K.docs)==null?void 0:Q.source}}};var X,Y,Z;p.parameters={...p.parameters,docs:{...(X=p.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    type: 'date'
+  }
+}`,...(Z=(Y=p.parameters)==null?void 0:Y.docs)==null?void 0:Z.source}}};var $,ee,re;m.parameters={...m.parameters,docs:{...($=m.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    type: 'datetime-local'
+  }
+}`,...(re=(ee=m.parameters)==null?void 0:ee.docs)==null?void 0:re.source}}};var ae,se,oe;i.parameters={...i.parameters,docs:{...(ae=i.parameters)==null?void 0:ae.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    type: 'email',
+    placeholder: "Email",
+    icon: "mail"
+  }
+}`,...(oe=(se=i.parameters)==null?void 0:se.docs)==null?void 0:oe.source}}};var te,ne,ce;l.parameters={...l.parameters,docs:{...(te=l.parameters)==null?void 0:te.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    type: 'month'
+  }
+}`,...(ce=(ne=l.parameters)==null?void 0:ne.docs)==null?void 0:ce.source}}};var pe,me,ie;u.parameters={...u.parameters,docs:{...(pe=u.parameters)==null?void 0:pe.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    type: 'number',
+    placeholder: "Phone number",
+    icon: "phone"
+  }
+}`,...(ie=(me=u.parameters)==null?void 0:me.docs)==null?void 0:ie.source}}};var le,ue,ge;g.parameters={...g.parameters,docs:{...(le=g.parameters)==null?void 0:le.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    type: 'number',
+    step: 0.5,
+    placeholder: "Floating point number"
+  }
+}`,...(ge=(ue=g.parameters)==null?void 0:ue.docs)==null?void 0:ge.source}}};var de,he,ye;d.parameters={...d.parameters,docs:{...(de=d.parameters)==null?void 0:de.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    type: 'password',
+    placeholder: "Type password"
+  }
+}`,...(ye=(he=d.parameters)==null?void 0:he.docs)==null?void 0:ye.source}}};var Se,fe,Te;h.parameters={...h.parameters,docs:{...(Se=h.parameters)==null?void 0:Se.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    type: 'range'
+  }
+}`,...(Te=(fe=h.parameters)==null?void 0:fe.docs)==null?void 0:Te.source}}};var De,be,xe;y.parameters={...y.parameters,docs:{...(De=y.parameters)==null?void 0:De.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    type: 'search'
+  }
+}`,...(xe=(be=y.parameters)==null?void 0:be.docs)==null?void 0:xe.source}}};var We,Ie,ve;S.parameters={...S.parameters,docs:{...(We=S.parameters)==null?void 0:We.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    type: 'tel'
+  }
+}`,...(ve=(Ie=S.parameters)==null?void 0:Ie.docs)==null?void 0:ve.source}}};var Ee,Pe,ke;f.parameters={...f.parameters,docs:{...(Ee=f.parameters)==null?void 0:Ee.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    type: 'time'
+  }
+}`,...(ke=(Pe=f.parameters)==null?void 0:Pe.docs)==null?void 0:ke.source}}};var qe,we,Le;T.parameters={...T.parameters,docs:{...(qe=T.parameters)==null?void 0:qe.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    type: 'url',
+    placeholder: 'type url'
+  }
+}`,...(Le=(we=T.parameters)==null?void 0:we.docs)==null?void 0:Le.source}}};var je,Ce,Fe;D.parameters={...D.parameters,docs:{...(je=D.parameters)==null?void 0:je.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    type: 'week'
+  }
+}`,...(Fe=(Ce=D.parameters)==null?void 0:Ce.docs)==null?void 0:Fe.source}}};var He,Ne,Oe;b.parameters={...b.parameters,docs:{...(He=b.parameters)==null?void 0:He.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    onEnterPress: () => {
+      console.log("Enter pressed");
+    }
+  }
+}`,...(Oe=(Ne=b.parameters)==null?void 0:Ne.docs)==null?void 0:Oe.source}}};var Re,Me,Ue;x.parameters={...x.parameters,docs:{...(Re=x.parameters)==null?void 0:Re.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    displayLoader: true,
+    icon: 'search'
+  }
+}`,...(Ue=(Me=x.parameters)==null?void 0:Me.docs)==null?void 0:Ue.source}}};const Xe=["Default","TextWithLabel","TextWithLabelOnTheSide","TextWithIcon","TextWithHoverIcon","WithError","WithInfo","Color","Date","DateTime","Email","Month","Number","Float","Password","Range","Search","Tel","Time","Url","Week","EnterPressed","WithSpinner"];export{c as Color,p as Date,m as DateTime,e as Default,i as Email,b as EnterPressed,g as Float,l as Month,u as Number,d as Password,h as Range,y as Search,S as Tel,o as TextWithHoverIcon,a as TextWithIcon,r as TextWithLabel,s as TextWithLabelOnTheSide,f as Time,T as Url,D as Week,t as WithError,n as WithInfo,x as WithSpinner,Xe as __namedExportsOrder,Qe as default};
---
+++ docs/assets/Pill.stories-5EviRgPB.js
@@ -0,0 +1,21 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{r as u}from"./index-D4lIrffr.js";import{P as m}from"./Modal-aZMi-ZjA.js";import{B as P}from"./Button-CpAfKxsB.js";/* empty css              */import"./Accordion-CM44lgGc.js";import"./Backdrop-B_dFhGSg.js";import"./Badge-DyX1sYip.js";import"./IconBadge-D9jC50y4.js";import"./Icon-1_RwmGu3.js";import"./DropdownButton-irs1yMoW.js";import"./DropdownNavButton-BJB-Par7.js";import"./ShareButton-CaP10xHi.js";import"./CopyButton-OOLUmhKo.js";import"./CsvButton-5VEuvKyO.js";import"./SortButton-DiZOgp43.js";import"./NavigateButton-D8qkO0to.js";import"./Checkbox-B1IiCnS1.js";/* empty css              */import"./Label-WHfwOnzY.js";import"./CsvInput-CsqwqRpP.js";/* empty css                   */import"./Spinner-Bp0FQ2cK.js";import"./ImageInput-D-cOjKP2.js";import"./Input-Cqf7JKmc.js";import"./PasswordInput-C25rVnwy.js";import"./Radio-Z5_Dey7K.js";import"./Select-DPMYrMY0.js";import"./Textarea-BFEtmDz2.js";import"./TextInputDropdown-stBR8hbx.js";import"./Li-BXwfRjCs.js";import"./Paper-7Qt7Ew3B.js";import"./Progress-CdK5Z50d.js";import"./Hr-DywUS52c.js";import"./ItemSlider-DXHSwitA.js";import"./RangeSlider-Bl7iMCYB.js";/* empty css               */import"./Slider-CI6O3bQt.js";import"./Featured-D1GSdbLo.js";import"./Star-2LGqRjU-.js";import"./Tabs-CD4dBFt4.js";import"./Table-BlH6J0Ay.js";import"./Card-sHLkWQUK.js";import"./FlippableCard-DWER4QIe.js";import"./ScrollFab-6zpsM4ZS.js";import"./RenderedNoteContent-DC26_sZC.js";const nt={component:m,title:"Data Display/Pill"},i=()=>{const[l,s]=u.useState(["Pill 1"]),c=()=>{const t=[...l];t.push("Pill "+(t.length+1)),s(t)},a=t=>{const o=[...l],d=o.indexOf(t);o.splice(d,1),s(o)};return r.jsxs("div",{className:"muncher-pill-story",children:[r.jsx(P,{variant:"tertiary",onClick:c,children:"Add pills"}),r.jsx("br",{}),r.jsx("br",{}),l.map(t=>r.jsx(m,{pillCloseHandler:()=>a(t),children:t},t))]})};i.__docgenInfo={description:"",methods:[],displayName:"Default"};var p,n,e;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  const [pills, setPills] = useState(["Pill 1"]);
+  const addPills = () => {
+    const currentPills = [...pills];
+    currentPills.push("Pill " + (currentPills.length + 1));
+    setPills(currentPills);
+  };
+  const removePill = (pill: string) => {
+    const currentPills = [...pills];
+    const index = currentPills.indexOf(pill);
+    currentPills.splice(index, 1);
+    setPills(currentPills);
+  };
+  return <div className="muncher-pill-story">
+
+            <Button variant={"tertiary"} onClick={addPills}>Add pills</Button>
+            <br /><br />
+            {pills.map(pill => <Pill key={pill} pillCloseHandler={() => removePill(pill)}>{pill}</Pill>)}
+
+        </div>;
+}`,...(e=(n=i.parameters)==null?void 0:n.docs)==null?void 0:e.source}}};const et=["Default"];export{i as Default,et as __namedExportsOrder,nt as default};
